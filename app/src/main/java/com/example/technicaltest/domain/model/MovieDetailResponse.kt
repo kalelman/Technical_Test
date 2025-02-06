4378d04f067ec86099c9e8d561e8c90eb31c29a9
Erick Rojas Perez <erick_rojas_perez@hotmail.com>
2025-02-06 13:28:02 -0600

    MovieDetailResponse and MovieResponse data class updated

--- app/src/main/java/com/example/technicaltest/domain/model/MovieDetailResponse.kt
+++ app/src/main/java/com/example/technicaltest/domain/model/MovieDetailResponse.kt
@@ -1,4 +1,14 @@
 package com.example.technicaltest.domain.model
 
-class MovieDetailResponse {
-}
+import com.google.gson.annotations.SerializedName
+
+data class MovieDetailResponse(
+    @SerializedName("results") val movies: List<Movie>
+)
+
+data class Movie(
+    @SerializedName("id") val id: Int,
+    @SerializedName("title") val title: String,
+    @SerializedName("poster_path") val posterUrl: String,
+    @SerializedName("vote_average") val rating: Double
+)
--- app/src/main/java/com/example/technicaltest/domain/model/MovieResponse.kt
+++ app/src/main/java/com/example/technicaltest/domain/model/MovieResponse.kt
@@ -1,4 +1,18 @@
 package com.example.technicaltest.domain.model
 
-class MovieResponse {
-}
+import com.google.gson.annotations.SerializedName
+
+data class MovieResponse(
+    @SerializedName("id") val id: Int,
+    @SerializedName("title") val title: String,
+    @SerializedName("overview") val overview: String,
+    @SerializedName("poster_path") val posterUrl: String,
+    @SerializedName("vote_average") val rating: Double,
+    @SerializedName("release_date") val releaseDate: String,
+    @SerializedName("genres") val genres: List<Genre>
+)
+
+data class Genre(
+    @SerializedName("id") val id: Int,
+    @SerializedName("name") val name: String
+)
